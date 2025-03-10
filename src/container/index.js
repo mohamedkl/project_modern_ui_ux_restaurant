f943e6d0e1f952877f9badff135873b6b8707037
Mohamed khaled <102388265+mohamedkl@users.noreply.github.com>
2025-03-10 23:24:13 +0200

    first commit

---
+++ src/container/index.js
@@ -0,0 +1,21 @@
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import Laurels from './Laurels/Laurels';
+import SpecialMenu from './Menu/SpecialMenu';
+import FindUs from './Findus/FindUs'
+
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
